2e41b287648c5252dd11edd66c00e14646adfb3c
Samson Awolaja <samson.awolaja@gmail.com>
2018-02-23 21:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  var numberOfItems = cart.length
   if (cardNumber !== null) {
     console.log(`Sorry, we don't have a credit card on file for you.`)
   } else {
