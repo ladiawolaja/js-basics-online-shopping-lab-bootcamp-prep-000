0583745feda12e5d991ff79aac2f49e8ae99547e
Samson Awolaja <samson.awolaja@gmail.com>
2018-02-23 21:57:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,12 +61,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  const cartTotal = total();
-  const cardNumber = Math.floor(Math.random()*1000000000)
   if (cardNumber !== null) {
     console.log(`Sorry, we don't have a credit card on file for you.`)
   } else {
-    console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`)
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
   }
   cart.length = 0
 }
