422b542b19ef9afb6b113bdf46b3b0b243428336
Samson Awolaja <samson.awolaja@gmail.com>
2018-02-23 20:31:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length; i++) { // loop
+  for (var i = 0; i < cart.length; i++) { 
     if (cart[i].hasOwnProperty(item)) {
       cart.splice(i,1);
       return cart
