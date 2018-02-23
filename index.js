004e9a7d78d1e4afac4d014c03b9da239ee6415e
Samson Awolaja <samson.awolaja@gmail.com>
2018-02-23 23:01:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,10 +61,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber !== undefined) {
-    console.log(`Sorry, we don't have a credit card on file for you.`)
-  } else {
+  if (cardNumber) {
     console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+  } else {
+    console.log(`Sorry, we don't have a credit card on file for you.`)
   }
   cart.length = 0
 }
