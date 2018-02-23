9ccd13798f1d3c088a9e71e0631bf8c786d28d91
Samson Awolaja <samson.awolaja@gmail.com>
2018-02-23 21:57:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,11 +62,11 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   const cartTotal = total();
-  const cardNumber = Math.floor(Math.random)
+  const cardNumber = Math.floor(Math.random()*1000000000)
   if (cardNumber !== null) {
     console.log(`Sorry, we don't have a credit card on file for you.`)
   } else {
-    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+    console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`)
   }
   cart.length = 0
 }
